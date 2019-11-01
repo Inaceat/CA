4baa476698212b44167bcf7de6f1c91df12d2752
Inaceat <inaceat@gmail.com>
2019-11-01 09:19:43 +0300

    Task1 implemented in Lab6.

--- Lab6/src/Task1.asm
+++ Lab6/src/Task1.asm
@@ -16,13 +16,14 @@ includelib kernel32.lib
 	taskMessage db "Enter x:", 0Dh, 0Ah;\r\n
 	taskMessageLength dd 10
 
-	numberString db 10+2 dup(0);For number & \r\n
+	numberInputString db 100+2 dup(0);For number & \r\n
 
 	resultMessage db "sqrt(abs(cos x + sin x)) = "
 	resultMessageLength dd 27
 
-	resultNumberString db 10 dup(0), 0Dh, 0Ah;For number & \r\n
-	resultNumberStringLength dd 12;Should be enough
+	resultNumberString db 100 dup(0), 0Dh, 0Ah;For number & \r\n
+	resultNumberStringMaxLength dd 102;Should be enough
+	resultNumberStringUsedLength dd 0
 
 .data?
 	inputHandle dd ?
@@ -31,7 +32,7 @@ includelib kernel32.lib
 	charsWritten dd ?
 	charsRead dd ?
 
-	xNumber dd ?
+	xNumber dq ?;now with float-point
 
 .code
 Task1 proc
@@ -54,32 +55,64 @@ Task1 proc
 	push outputHandle
 	call WriteConsole
 
-;Read x, assuming it is valid 32-bit value
+;Read x, assuming it is valid floating-point number
 	push NULL
 	push offset charsRead
-	push 10+2
-	push offset numberString
+	push 100+2
+	push offset numberInputString
 	push inputHandle
 	call ReadConsole
 
 	;Put \0 after last digit
 	mov EAX, charsRead
-	mov [numberString + EAX - 2], 0
+	mov [numberInputString + EAX - 2], 0
 
 	;Convert from string to number
-	push offset numberString
-	call atodw
-	mov xNumber, EAX
+	push offset xNumber
+	push offset numberInputString
+	call StrToFloat
 
 ;Calculate sqrt(abs(cos x + sin x))
+	finit
+
+	fld xNumber
+	fsin
+	;Now ST has sin x
+
+	fld xNumber
+	fcos
+	;Now ST has cos x
+	;And ST(1) has sin x
+
+	fadd ST(0), ST(1)
+	;Now ST has sin x + cos x
 	
-	mov EAX, 41
+	fabs
+	;Now ST has abs(sin x + cos x)
+
+	fsqrt
+	;Now ST has sqrt(abs(sin x + cos x))
+	
+	fstp xNumber
 
 ;Print result
 	;Convert resulting number to string
 	push offset resultNumberString
-	push EAX
-	call dwtoa
+	push dword ptr xNumber + 4
+	push dword ptr xNumber
+	call FloatToStr
+
+	;Get number representation length
+	push offset resultNumberString
+	call lstrlen
+	;Now EAX has number of used bytes in resultString
+	;Add \r\n to string and to used length
+	mov [resultNumberString + EAX], 0Dh
+	mov [resultNumberString + EAX + 1], 0Ah
+	;Add \0
+	mov [resultNumberString + EAX + 2], 0
+	add EAX, 3
+	mov resultNumberStringUsedLength, EAX
 
 	;Print result description message
 	push NULL                
@@ -92,14 +125,14 @@ Task1 proc
 	;Print result number
 	push NULL                
 	push offset charsWritten
-	push resultNumberStringLength
+	push resultNumberStringUsedLength
 	push offset resultNumberString
 	push outputHandle
 	call WriteConsole
 
 	;Clear result number string
 	mov ECX, 0
-	mov EBX, resultNumberStringLength
+	mov EBX, resultNumberStringMaxLength
 	sub EBX, 2;\r\n should remain, so Length - 2
 	ClearingCycle:
 		cmp ECX, EBX
